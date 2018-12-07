77c7b715603322f7822a2e5f7721841e210d247e
dat <datdta95@gmail.com>
2018-12-07 18:47:58 +0700

    file main app

---
+++ src/main/java/com/thinkpad/employee_management_springboot/EmployeeManagementSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.thinkpad.employee_management_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSpringBootApplication.class, args);
+	}
+}
