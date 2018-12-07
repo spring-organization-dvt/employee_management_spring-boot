ad23b2d6fbe38cc029974d11f05d8c757af7d149
dat <datdta95@gmail.com>
2018-12-08 01:43:30 +0700

    update file main

--- src/main/java/com/thinkpad/employee_management_springboot/EmployeeManagementSpringBootApplication.java
+++ src/main/java/com/thinkpad/employee_management_springboot/EmployeeManagementSpringBootApplication.java
@@ -1,10 +1,25 @@
 package com.thinkpad.employee_management_springboot;
 
+import com.thinkpad.employee_management_springboot.service.EmployeeService;
+import com.thinkpad.employee_management_springboot.service.EmployeeServiceImpl;
+import com.thinkpad.employee_management_springboot.service.GroupEmployeeService;
+import com.thinkpad.employee_management_springboot.service.GroupEmployeeServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeManagementSpringBootApplication {
+
+	@Bean
+	public EmployeeService employeeService() {
+		return new EmployeeServiceImpl();
+	}
+
+	@Bean
+	public GroupEmployeeService groupEmployeeService() {
+		return new GroupEmployeeServiceImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSpringBootApplication.class, args);
