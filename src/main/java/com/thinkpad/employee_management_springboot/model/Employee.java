91a4dfaaf1aecf601cc55df96499d6308336f394
dat <datdta95@gmail.com>
2018-12-08 01:43:12 +0700

    update file model employee

--- src/main/java/com/thinkpad/employee_management_springboot/model/Employee.java
+++ src/main/java/com/thinkpad/employee_management_springboot/model/Employee.java
@@ -1,6 +1,10 @@
 package com.thinkpad.employee_management_springboot.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "employee")
@@ -8,9 +12,13 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotEmpty(message = "Tên không được để trống")
+    @Size(min = 2,max = 30,message = "Tên có độ dài ngắn nhất là 2 kí tự và không dài quá 30 kí tự")
     private String name;
+    @NotEmpty(message = "Giới tính chưa được chọn!")
     private String gender;
     private String dayOfBirth;
+    @Pattern(regexp = "^0[38759][0-9]{8}$",message = "Số điện thoại không đúng!")
     private String phoneNumber;
     private String idCard;
     private String email;
